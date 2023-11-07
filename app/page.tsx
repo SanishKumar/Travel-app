ac8414632e6baadfbc246d72a9f021077e22b47e
SanishKumar <18sanishkumar@gmail.com>
2023-11-07 22:52:06 +0530

    Hero Section(Mini Camp)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image'
+import Hero from './components/Hero'
+import Camp from './components/Camp'
+import Guide from './components/Guide'
+import Features from './components/Features'
+import GetApp from './components/GetApp'
 
 export default function Home() {
   return (
-    <h1 className='font-bold text-teal-500 text-2xl'>
-      Hello World
-    </h1>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
   )
 }
