5535bec47eae03fb6ea070be8d7058ec895d3eb4
SanishKumar <18sanishkumar@gmail.com>
2023-12-02 19:06:50 +0530

    Footer....

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,4 +8,13 @@ const Footer = () => {
   )
 }
 
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  )
+}
+
 export default Footer
